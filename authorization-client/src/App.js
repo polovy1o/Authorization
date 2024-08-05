ad75979f69f8c3469c94c0de713a26c37695ae2b
polovy1o <polovy1o@gmail.com>
2024-08-05 14:57:06 +0300

    Initial commit

---
+++ authorization-client/src/App.js
@@ -0,0 +1,25 @@
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { Link } from './components/ui/Link';
+
+function App() {
+    return ( 
+        <div>
+            <AppBar position="static" color="inherit" sx={{ boxShadow: "0 0 1px 2px rgba(0, 0, 0, 0.1)" }}>
+                <Container>
+                    <Toolbar>
+                        <Link to="/" style={{color:"black"}}>
+                            <Typography variant="h6" component="div">
+                                SomeWebsite
+                            </Typography>
+                        </Link>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+                <Outlet />
+        </div>
+    );
+}
+
+export default App;
